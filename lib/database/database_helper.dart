4598f92d7f6d187f20bbab6be2fdb61b24d063bb
Hugo Sens <victor.sens@unifebe.edu.br>
2026-05-17 16:57:08 -0300

    ajuste codigo de barra teste

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -19,9 +19,23 @@ class DatabaseHelper {
     String path = join(await getDatabasesPath(), 'picking_database.db');
     return await openDatabase(
       path,
-      version: 1,
+      version: 2,
       onCreate: _onCreate,
+      onUpgrade: _onUpgrade,
     );
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    if (oldVersion < 2) {
+      await db.update(
+        'usuario',
+        {
+          'codigo_barra_usuario': '1234567899992'
+        },
+        where: 'codigo_usuario = ?',
+        whereArgs: [1],
+      );
+    }
   }
 
   Future _onCreate(Database db, int version) async {
